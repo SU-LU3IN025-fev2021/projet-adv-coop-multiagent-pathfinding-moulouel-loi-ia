226da867536f6f3e113f436d2b3ce5b9f12c6ea5
myriem-moulouel <60098131+myriem-moulouel@users.noreply.github.com>
2021-03-30 14:12:25 +0200

    Update grid2D.py

--- adv_coop_multiagent_pathfinding/search/grid2D.py
+++ adv_coop_multiagent_pathfinding/search/grid2D.py
@@ -84,7 +84,7 @@ class ProblemeGrid2D(Probleme):
             """
         s=""
         (x,y)= etat
-        s+=str(x)+str(y)
+        s+=str(x)+'_'+str(y)
         return s
         
     def h_value(self,e1,e2):
@@ -98,5 +98,4 @@ class ProblemeGrid2D(Probleme):
 
 
             
-
 
