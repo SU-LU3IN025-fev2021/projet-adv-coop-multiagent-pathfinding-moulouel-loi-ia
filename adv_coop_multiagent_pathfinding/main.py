f83b76833f83c7a8f10d83d7b3d6b38a5eaff6dc
myriem-moulouel <60098131+myriem-moulouel@users.noreply.github.com>
2021-03-30 14:09:32 +0200

    Update main.py

--- adv_coop_multiagent_pathfinding/main.py
+++ adv_coop_multiagent_pathfinding/main.py
@@ -76,11 +76,10 @@ def main():
     #init('map1_2equipes_2joueurs')
     #init('map1_2equipes_5joueurs')
     #init('map2_2equipes_1joueur')
-    init('map2_2equipes_2joueurs')
-    #init('map2_2equipes_3joueurs')
+    #init('map2_2equipes_2joueurs')
+    init('map2_2equipes_3joueurs')
     #init('map2_2equipes_5joueurs')
     #init('map2_2equipes_10joueurs')
-    #init('map2_croisement')
 
 #-------------------------------
 
@@ -146,21 +145,21 @@ def main():
 
     #random.seed(42)
     objectifs = goalStates
-    #random.shuffle(objectifs)
+    random.shuffle(objectifs)
 
     # Objectifs de l'equipe Rouge
     print("Objectifs de l'équipe Rouge")
     for i in range(nbPlayersEquipe):
         objectifs_equipeR.append(objectifs[i])
         print("\tJoueur jR" + str(i), objectifs_equipeR[i])
-    random.shuffle(objectifs_equipeR)
+    #random.shuffle(objectifs_equipeR)
 
     # Objectifs de l'equipe Bleue
     print("\nObjectifs de l'équipe Bleue")
     for i in range(nbPlayersEquipe):
         objectifs_equipeB.append(objectifs[nbPlayersEquipe+i])
         print("\tJoueur jB" + str(i), objectifs_equipeB[i])
-    random.shuffle(objectifs_equipeR)
+    #random.shuffle(objectifs_equipeR)
 
 
 
@@ -196,8 +195,8 @@ def main():
     # 2 équipes de <nbPlayersEquipe> agents chacune
     # Strategies applicables : 
     #       1) Local Repair A* : A* indépendants au sein d'une équipe, recalcul du chemin en cas de collision
-    #       2)
-    #       3)
+    #       2) Collaborative Pathfinding with A* : les agents d'une meme équipe evitent de croiser leurs chemins
+    #       3) MinMax : les agents d'une équipe se deplacent en fonction de leur prévision des pas adversaires
     # 
     #-------------------------------
 
@@ -207,9 +206,8 @@ def main():
     MINMAX = 3
 
     # Choix de la strategie à employer pour chaque équipe
-    strategieB = LOCAL_REPAIR
+    strategieB = MINMAX
     strategieR = LOCAL_REPAIR
-
     #-------------------------------
     # Boucle principale de déplacements 
     #-------------------------------
@@ -218,31 +216,21 @@ def main():
     posPlayersB = initStates_equipeB 
     posPlayersR = initStates_equipeR
 
-    # Listes des scores associés à chaque joueur
+    # Listes et variables pour la comptabilisation des scores associés à chaque joueur
     scoreB = [0]*nbPlayersEquipe 
     scoreR = [0]*nbPlayersEquipe
+    ex_posPlayersB = list(posPlayersB)
+    ex_posPlayersR = list(posPlayersR)
+    nbPas_equipeB = 0
+    nbPas_equipeR = 0
 
 
-    #listes des joueurs en attente
-    B_en_attente = []
-    
-    R_en_attente = []
-
-
-    pathB = [[coup] for coup in posPlayersB]
-    pathR = [[coup] for coup in posPlayersR]
 
 
 
     for tour_jeu in range(iterations):
 
         print("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   Iteration de jeu n." + str(tour_jeu) + "   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-
-        for i in range(len(equipeB)):
-            B_en_attente.append(i)
-
-        for i in range(len(equipeR)):
-            R_en_attente.append(i)
 
         # on fait bouger chaque équipe séquentiellement :
         
@@ -251,13 +239,13 @@ def main():
 
         if strategieB == LOCAL_REPAIR:
             posPlayersB = probleme.localRepair_astar(equipeB,posPlayersB,posPlayersR,scoreB)
-            print(posPlayersB)
+    
         if strategieB == COLLAB_PATH_FINDING:
             posPlayersB = probleme.collaborativePathfinding(equipeB,posPlayersB,posPlayersR,objectifs_equipeB,initStates_equipeB,scoreB)
-            print(posPlayersB)
+         
         if strategieB == MINMAX:
-            posPlayersB = probleme.MinMax(g, equipeB, equipeR, posPlayersB, posPlayersR, 1)
-            print(posPlayersB)
+            posPlayersB = probleme.MinMax(g, equipeB, equipeR, posPlayersB, posPlayersR, 2)
+         
 
         # Deplacement sur les cases choisies   
         print("\nDeplacements définis pour l'équipe BLEUE :")        
@@ -270,12 +258,12 @@ def main():
         for i in range(nbPlayersEquipe):
             if scoreB[i]==0 and posPlayersB[i]==objectifs_equipeB[i]:
                 scoreB[i] += 1
-                B_en_attente.remove(i)
                 print("Le joueur jB" + str(i) + " a atteint son but!")
 
-        # Les bleus ont gagné?
-        if sum(scoreB) == nbPlayersEquipe:
-            break
+            if posPlayersB[i] != ex_posPlayersB[i]:
+                nbPas_equipeB +=1
+                ex_posPlayersB[i] = posPlayersB[i]
+
 
 
         print("\n----------------------------------------------------------------------------------------------") 
@@ -285,10 +273,12 @@ def main():
 
         if strategieR == LOCAL_REPAIR:
             posPlayersR = probleme.localRepair_astar(equipeR,posPlayersR,posPlayersB,scoreR)
+
         if strategieR == COLLAB_PATH_FINDING:
             posPlayersR = probleme.collaborativePathfinding(equipeR,posPlayersR,posPlayersB,objectifs_equipeR,initStates_equipeR,scoreR)
+
         if strategieR == MINMAX:
-            posPlayersR = probleme.MinMax(g, equipeR, equipeB, posPlayersR, posPlayersB, 2)
+            posPlayersR = probleme.MinMax(g, equipeR, equipeR, posPlayersR, posPlayersB, 2)
         
         # Deplacement sur les cases choisies   
         print("\nDeplacements définis pour l'équipe ROUGE :")           
@@ -301,23 +291,25 @@ def main():
         for i in range(nbPlayersEquipe):
             if scoreR[i]==0 and posPlayersR[i]==objectifs_equipeR[i]:
                 scoreR[i] += 1
-                R_en_attente.remove(i)
                 print("Le joueur jR" + str(i) + " a atteint son but!")
 
-        # Les rouges ont gagné?
-        if sum(scoreR) == nbPlayersEquipe:
+            if posPlayersR[i] != ex_posPlayersR[i]:
+                nbPas_equipeR +=1
+                ex_posPlayersR[i] = posPlayersR[i]
+
+        # Les bleus ont gagné?
+        if sum(scoreB) == nbPlayersEquipe and sum(scoreB)>sum(scoreR):
             break
 
-        for i in range(len(equipeB)):
-            if posPlayersB[i] not in pathB[i]:
-                pathB[i].append(posPlayersB[i])
-        for i in range(len(equipeR)):
-            if posPlayersR[i] not in pathR[i]:
-                pathR[i].append(posPlayersR[i])
+        # Les rouges ont gagné?
+        if sum(scoreR) == nbPlayersEquipe and sum(scoreR)>sum(scoreB):
+            break
+
+        if sum(scoreB) == nbPlayersEquipe or sum(scoreR) == nbPlayersEquipe:
+            break
 
 
 
-        
         # On passe a l'iteration suivante du jeu
         game.mainiteration()
 
@@ -327,8 +319,8 @@ def main():
     #-------------------------------                
 
     print("\n----------------------------------------------------------------------------------------------\n")       
-    print ("Scores de l'équipe Bleue :", scoreB)
-    print ("Scores de l'équipe Rouge :", scoreR)
+    print ("Scores de l'équipe Bleue :", scoreB, "\tNombre totale de pas effectués =", nbPas_equipeB)
+    print ("Scores de l'équipe Rouge :", scoreR, "\tNombre totale de pas effectués =", nbPas_equipeR)
 
     resultat = sum(scoreB) - sum(scoreR)
     if resultat > 0:
@@ -336,41 +328,23 @@ def main():
     elif resultat < 0:
         print("\nL'équipe Rouge a gagné!")
     else: # si scoreB.sum() = scoreR.sum()
-        taille_chemin1 = 0
-        taille_chemin2 = 0
-        for i in range(len(equipeB)):
-            taille_chemin1+=(len(pathB[i]))
-        for i in range(len(equipeR)):
-            taille_chemin2+=(len(pathR[i]))
-
-        if taille_chemin1 > taille_chemin2:
-            print("equipe B et R ont le meme score, mais R a fait moins de trajet => R gagne")
-        elif taille_chemin2 > taille_chemin1:
-            print("equipe B et R ont le meme score, mais B a fait moins de trajet => B gagne")
+        if nbPas_equipeB > nbPas_equipeR:
+            print("\nLes équipes B et R ont eu le meme score, mais l'équipe R gagne car elle a fait moins de trajet!")
+        elif nbPas_equipeR > nbPas_equipeB:
+            print("\nLes équipes B et R ont eu le meme score, mais l'équipe B gagne car elle a fait moins de trajet!")
         else:
-            print("\nLes équipes Bleue et Rouge ont eu le même score et ont parcouru la meme distance!") 
+            print("\nLes équipes Bleue et Rouge ont eu le même score et ont parcouru la meme distance! Parité!") 
 
 
     # Attente de 5 secondes avant de fermer la fenetre graphique
-    time.sleep(5) 
+    #time.sleep(5) 
     pygame.quit()
+    return (sum(scoreB), sum(scoreR))
     
-    del(pathB)
-    del(pathR)
-    #gc.collect()
-    #-------------------------------
-    print("resultat: ",resultat)
-    return (sum(scoreB),sum(scoreR))
-    
+
+
 if __name__ == '__main__':
-    filename = "nom_du_fichier" 
-    with open(filename, 'w') as f: 
-        f.write("iteration\tscoreB\tscoreR\n")
-        for i in range(2):
-            scoreB,scoreR = main()
-            
-            f.write(str(i)+"\t"+str(scoreB)+"\t"+str(scoreR)+"\n")
-    
+    main()
     
     
     
